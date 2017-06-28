ff5ad195588c80287027582f56b88a1db2b4e460
Greg Guy <javag57@gmail.com>
2017-06-28 16:34:09 +0200

    commit 3

--- java.main/vzap/greg/MySQL_Connection.java
+++ java.main/vzap/greg/MySQL_Connection.java
@@ -4,16 +4,37 @@ import java.io.FileInputStream;
 import java.sql.*;
 import java.util.Properties;
 
+// TODO: Auto-generated Javadoc
+/**
+ * The Class MySQL_Connection.
+ */
 public class MySQL_Connection
 {
+	
+	/** The username. */
 	private String username;
+	
+	/** The password. */
 	private String password;
+	
+	/** The database name. */
 	private String databaseName;
+	
+	/** The ip address. */
 	private String ipAddress;
+	
+	/** The port number. */
 	private String portNumber;
+	
+	/** The connection. */
 	private Connection connection;
+	
+	/** The url. */
 	private String url;
 
+	/**
+	 * Instantiates a new my SQ L connection.
+	 */
 	public MySQL_Connection()// constructor
 	{
 		Properties dbProperties = new Properties();
@@ -46,19 +67,42 @@ public class MySQL_Connection
 		}
 
 	}
+	
+	/**
+	 * Gets the user.
+	 *
+	 * @return the user
+	 */
 	public String getUser()
 	{
 		return username;
 	}
+	
+	/**
+	 * Gets the database name.
+	 *
+	 * @return the database name
+	 */
 	public String getDatabaseName()
 	{
 		return databaseName;
 	}
+	
+	/**
+	 * Gets the connection.
+	 *
+	 * @return the connection
+	 */
 	public Connection getConnection()
 	{
 		return connection;
 	}
 
+	/**
+	 * Close connection.
+	 *
+	 * @return true, if successful
+	 */
 	public boolean closeConnection()
 	{
 		if (connection != null)
@@ -79,6 +123,11 @@ public class MySQL_Connection
 		return true;
 	}
 
+	/**
+	 * The main method.
+	 *
+	 * @param args the arguments
+	 */
 	public static void main(String[] args)
 	{
 		MySQL_Connection mySqlConn = new MySQL_Connection();
